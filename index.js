e65b629565e80ff63d203aeda67ce71ce8dd13dc
Suman Singh <suman.singh@intl.verizon.com>
2018-02-22 11:48:01 +0530

    added passport js dependency

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // common js modules in the server as nodejs has only support for modules
 // node js do not have in built support for es2015 modules natively yet.
 
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // instantiate an express app
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 // we are using heroku hosting, so whenever heroku runs it injects the PORT value to the environment variables.
 const PORT = process.env.PORT || 5000;
@@ -14,12 +18,7 @@ app.get("/", (req, res) => {
   res.send({ hi: "hello there, how are you doing?" });
 });
 
-app.get("/test", (req, res) => {
-  res.send({
-    data: "test",
-    status: "success"
-  });
-});
+
 
 // start express app to listen to this port.
 app.listen(PORT);
