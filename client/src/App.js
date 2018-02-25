d552bbf70d6533a8b3b1a5fe2561833515d25407
suman <spyder@SpYDeR.local>
2018-02-25 22:43:58 +0530

    eject with airbnb

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Emaily Application</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-intro">
+          Click <a href="/auth/google"> Here </a> to sign into Google.
         </p>
       </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-env browser, jest */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
