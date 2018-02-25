7291c3d08b585d1ac0998ef0da3ed625cac07eff
suman <spyder@SpYDeR.local>
2018-02-25 17:30:09 +0530

    dev/prod setup

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+// Logic that checks if dev/prod mode and return the values appropritely
+
+/**
+ * Since we are using Heroku to deploy the application, it automatically sets the value of NODE_EMV in
+ * environment variable. i.e.,
+ * process.env.NODE_ENV
+ */
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("prod");
+} else {
+  module.exports = require("dev");
+}
