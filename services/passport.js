6ee86cfd4b868a82253e88921c4315a05c0685e0
suman <spyder@SpYDeR.local>
2018-02-25 17:46:29 +0530

    google strategy proxy

--- services/passport.js
+++ services/passport.js
@@ -27,7 +27,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accesToken, refreshToken, profile, done) => {
       // query MongoDB to see if this person alreay has this profileID. if present, no record required.
