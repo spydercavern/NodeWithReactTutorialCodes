d7a27b82b2f1d5bd4a830b62b04fd64213712ff9
suman <spyder@SpYDeR.local>
2018-02-25 23:40:45 +0530

    add proxy refactor the promise to async/await

--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,8 @@
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require("mongoose");
-const keys = require("../config/keys");
-const User = mongoose.model("users");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+const User = mongoose.model('users');
 
 /**
  * serialize the user using passport. this is a user record id created by
@@ -16,10 +16,9 @@ passport.serializeUser((user, done) => {
  * Deserializion logic to fetch the mongodb user record from the id
  * that is retrieved from the Cookie
  */
-passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
-    done(null, user);
-  });
+passport.deserializeUser(async (id, done) => {
+  let user = await User.findById(id);
+  done(null, user);
 });
 
 passport.use(
@@ -27,24 +26,24 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
+      callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accesToken, refreshToken, profile, done) => {
+    async (accesToken, refreshToken, profile, done) => {
       // query MongoDB to see if this person alreay has this profileID. if present, no record required.
       // User.findOne returns a promise,
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        // if no user, existingUser is null
-        if (existingUser) {
-          // we already have a record with the profileId
-          done(null, existingUser);
-        } else {
-          // Create a new User( record in the users collection in mongoDb)
-          let newUser = new User({ googleId: profile.id });
-          // we will need to save(persist) this new user to save it to the mongoDB
-          newUser.save().then(user => done(null, user));
-        }
-      });
+      const existingUser = await User.findOne({ googleId: profile.id });
+      // if no user, existingUser is null
+      if (existingUser) {
+        // we already have a record with the profileId
+        done(null, existingUser);
+      } else {
+        // Create a new User( record in the users collection in mongoDb)
+        const newUser = new User({ googleId: profile.id });
+        // we will need to save(persist) this new user to save it to the mongoDB
+        const user = await newUser.save();
+        done(null, user);
+      }
     }
   )
 );
