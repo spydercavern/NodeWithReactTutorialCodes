3f171afef8b69e3ee9006263f23c893ffc470b7d
Suman Singh <suman.singh@intl.verizon.com>
2018-02-26 19:03:59 +0530

    basic application with redux, react-router dependency added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  componentDidMount() {}
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Emaily Application</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          Click <a href="/auth/google"> Here </a> to sign into Google.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => <div> test </div>;
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,5 @@
-/* eslint-env browser, jest */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.querySelector('#root'));
